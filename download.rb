d5f5814bb7729585af7273caa0506a44a6aaeea3
utsushiiro <ozeki.haru@gmail.com>
2018-03-26 02:45:57 +0900

    historyのmongodbへの保存を実装

--- download.rb
+++ download.rb
@@ -1,4 +1,5 @@
 require 'slack'
+require 'mongo'
 
 Slack.configure do |config|
   config.token = ENV['SLACK_HISTORY_DOWNLOADER']
@@ -7,4 +8,13 @@ end
 client = Slack::Client.new
 channel = client.channels_list['channels'].map { |i| [i['name'], i['id']] }.to_h
 history = client.channels_history(channel: channel['general'])
-puts history
+if history['ok']
+  messages = history['messages']
+else
+  puts 'fail to get history'
+  exit(0)
+end
+
+client = Mongo::Client.new 'mongodb://127.0.0.1:27017/slack'
+collection = client[:history]
+collection.insert_many messages
