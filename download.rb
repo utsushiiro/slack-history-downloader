028aae626228f50d05f14cfbe133c189823688b3
utsushiiro <ozeki.haru@gmail.com>
2018-03-26 16:45:40 +0900

    重複するメッセージを保存しないように変更

--- download.rb
+++ download.rb
@@ -8,6 +8,8 @@ end
 client = Slack::Client.new
 channel = client.channels_list['channels'].map { |i| [i['name'], i['id']] }.to_h
 history = client.channels_history(channel: channel['general'])
+
+messages = []
 if history['ok']
   messages = history['messages']
 else
@@ -17,4 +19,20 @@ end
 
 client = Mongo::Client.new 'mongodb://127.0.0.1:27017/slack'
 collection = client[:history]
-collection.insert_many messages
+
+new_messages = []
+stored_timestamps = collection.find.sort(ts: -1)
+                              .projection(ts: 1, _id: 0).map { |i| i['ts'] }
+
+def exist?(message, stored_timestamps)
+  stored_timestamps.each do |stored_timestamp|
+    return true if message['ts'] == stored_timestamp
+  end
+  false
+end
+
+messages.each do |message|
+  new_messages << message unless exist?(message, stored_timestamps)
+end
+
+collection.insert_many new_messages
