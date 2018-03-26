bf51dc44acea0de71b7e533c8a57962d037220fd
utsushiiro <ozeki.haru@gmail.com>
2018-03-27 01:13:36 +0900

    Storeクラスを作成

--- download.rb
+++ download.rb
@@ -1,25 +1,9 @@
 require 'mongo'
 require './client'
+require './store'
 
 slack_client = Client.new
-messages = slack_client.get_channel_history('general')
+history = slack_client.get_channel_history('general')
 
-client = Mongo::Client.new 'mongodb://127.0.0.1:27017/slack'
-collection = client[:history]
-
-new_messages = []
-stored_timestamps = collection.find.sort(ts: -1)
-                              .projection(ts: 1, _id: 0).map { |i| i['ts'] }
-
-def exist?(message, stored_timestamps)
-  stored_timestamps.each do |stored_timestamp|
-    return true if message['ts'] == stored_timestamp
-  end
-  false
-end
-
-messages.each do |message|
-  new_messages << message unless exist?(message, stored_timestamps)
-end
-
-collection.insert_many new_messages
+store = Store.new
+store.update_channel_history(history)
---
+++ store.rb
@@ -0,0 +1,30 @@
+require 'mongo'
+
+class Store
+  def initialize
+    @client = Mongo::Client.new 'mongodb://127.0.0.1:27017/slack'
+    @history_collection = @client[:history]
+  end
+
+  def update_channel_history(history)
+    @history_collection.insert_many(get_new_messages(history))
+  end
+
+  def get_new_messages(messages)
+    new_messages = []
+    stored_timestamps = @history_collection.find
+                          .sort(ts: -1)
+                          .projection(ts: 1, _id: 0)
+                          .map { |i| i['ts'] }
+    messages.each do |message|
+      new_messages << message unless message_stored?(message, stored_timestamps)
+    end
+  end
+
+  def message_stored?(message, stored_timestamps)
+    stored_timestamps.each do |stored_timestamp|
+      return true if message['ts'] == stored_timestamp
+    end
+    false
+  end
+end
