0e44327c56fed9e82e12924bdcbb3ec1d4090dcf
utsushiiro <ozeki.haru@gmail.com>
2018-03-27 18:49:18 +0900

    historyダウンロードの際のページングを有効に

--- client.rb
+++ client.rb
@@ -1,6 +1,9 @@
 require 'slack'
 
 class Client
+  HISTORY_PAGE_SIZE = 1000
+  MAX_HISTORY_PAGE_NUM = 10
+
   def initialize
     @client = Slack::Client.new token: ENV['SLACK_HISTORY_DOWNLOADER']
     @channel_list = @client.channels_list['channels']
@@ -9,13 +12,29 @@ class Client
 
   def get_channel_history(channel_name)
     channel_id = @channel_name_to_id_table[channel_name]
-    history = @client.channels_history(channel: channel_id)
+    history = @client.channels_history(channel: channel_id, count: HISTORY_PAGE_SIZE)
+    message_stack = history['messages']
+    page = 1
 
     unless history['ok']
       puts 'fail to get history'
       exit(0)
     end
 
-    history['messages']
+    while history['has_more']
+      break if page > MAX_HISTORY_PAGE_NUM
+
+      oldest = history['messages'][0]['ts']
+      history['messages'].each do |message|
+        ts = message['ts']
+        oldest = ts if oldest > ts
+      end
+
+      history = @client.channels_history(channel: channel_id, latest: oldest, count: HISTORY_PAGE_SIZE)
+      message_stack.push(history['messages'])
+      page += 1
+    end
+
+    message_stack
   end
 end
